866cc542d39c56b4a0e996b2620316a6ecacf48f
cen1 <klemenferjancic@hotmail.com>
2016-05-18 00:33:09 +0200

    Keycloak env fix

--- deployment/keycloak/run.sh
+++ deployment/keycloak/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --name crossbuild-keycloak --link crossbuild-postgres:postgres -p 8082:8080 -e POSTGRES_DATABASE=keycloak -e POSTGRES_USER=keycloak -e POSTGRES_PASSWORD=keycloak -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin jboss/keycloak-postgres
+docker run -d --name crossbuild-keycloak --link crossbuild-postgres:postgres -p 8443:8443 -e POSTGRES_DATABASE=keycloak -e POSTGRES_USER=keycloak -e POSTGRES_PASSWORD=crossbuild2016ae -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=crossbuild2016ae crossbuild-keycloak
